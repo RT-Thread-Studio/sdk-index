1478a966b481c2ec9ffc530bdeb1f56c3130c554
SummerGift <summergift2019@gmail.com>
2020-05-12 15:54:48 +0800

    [add] sync sdk

--- tools/indexing.py
+++ tools/indexing.py
@@ -14,7 +14,7 @@ import logging
 import os
 from jsonschema import RefResolver, Draft7Validator, validate
 import requests
-from .sync_sdk import is_master_repo
+from tools.sync_sdk import is_master_repo
 
 
 def init_logger():
