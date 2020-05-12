962b9d697f3e72589711cd87986c666a8bdf3ede
SummerGift <summergift2019@gmail.com>
2020-05-12 16:12:30 +0800

    [add] sync sdk

--- tools/sync_sdk.py
+++ tools/sync_sdk.py
@@ -1,11 +1,25 @@
 import os
+import json
+import logging
+import requests
 
 
 def is_master_repo():
-    sys_environment = os.environ
-    if 'IS_MASTER_REPO' in sys_environment:
+    if 'IS_MASTER_REPO' in os.environ:
         return True
     else:
         return False
 
 
+def update_sdk_index(new_index):
+    if 'UPDATE_SDK_INDEX_ADDRESS' in os.environ:
+        try:
+            r = requests.post(os.environ["UPDATE_SDK_INDEX_ADDRESS"], data=json.dumps(new_index))
+
+            if r.status_code == requests.codes.ok:
+                print("hello")
+
+        except Exception as e:
+            print('Error message:%s' % e)
+    else:
+        pass
