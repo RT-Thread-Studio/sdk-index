13aecb1499b9aa6099dfce295298211c0b7b702f
liukang <liukang@rt-thread.com>
2020-05-13 16:34:35 +0800

    [update] logging

--- tools/csp_check/gen_test_case.py
+++ tools/csp_check/gen_test_case.py
@@ -79,7 +79,7 @@ def get_generate_result(json_name):
 
 
 def get_import_result(cmd_pre, project_name):
-    cmd = cmd_pre + ' -import "file:/rt-thread/workspace/{0}"'.format(project_name)
+    cmd = cmd_pre + ' -import "file:/rt-thread/workspace/{0}" 2>/dev/null'.format(project_name)
     result = execute_command(cmd)
     if result.find("Create") != -1:
         print("import result : {0}".format(result))
@@ -89,7 +89,7 @@ def get_import_result(cmd_pre, project_name):
         
         
 def get_build_result(cmd_pre, project_name):
-    cmd = cmd_pre + " -cleanBuild '{0}'".format(project_name)
+    cmd = cmd_pre + " -cleanBuild '{0}' 2>/dev/null".format(project_name)
     result = execute_command(cmd)
     if result.find("Finished building target: rtthread.elf") != -1:
         print("import result : {0}".format(result))
