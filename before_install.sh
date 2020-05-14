dc52013ce7fa35c9a15251265d69958532317583
SummerGift <summergift2019@gmail.com>
2020-05-14 09:54:30 +0800

    [update] ci

--- before_install.sh
+++ before_install.sh
@@ -4,9 +4,9 @@ then
 else
         echo "Master trigger"
         openssl aes-256-cbc -K $encrypted_3b9f0b9d36d1_key -iv $encrypted_3b9f0b9d36d1_iv -in secrets.tar.enc -out secrets.tar -d
-        && tar xvf secrets.tar
-        && eval $(ssh-agent -s)
-        && chmod 600 id_rsa
-        && ssh-add id_rsa
-        && cp -f known_hosts ~/.ssh
+        tar xvf secrets.tar
+        eval $(ssh-agent -s)
+        chmod 600 id_rsa
+        ssh-add id_rsa
+        cp -f known_hosts ~/.ssh
 fi
