aac3c713b8f39b5cfe6e831b8432c703693a751f
liukang <liukang@rt-thread.com>
2020-05-15 10:03:12 +0800

    [update] logging

--- tools/main.py
+++ tools/main.py
@@ -122,7 +122,8 @@ class StudioSdkManagerIndex:
     @staticmethod
     def csp_to_test(csp_result):
         if len(csp_result) is not 1:
-            logging.info("You can commit only one csp package once, please modify the index you commit.")
+            logging.info("You can commit only one csp package once, so you should modify the index you commit.")
+            logging.info("Please check the list following:")
             logging.info(csp_result)
             exit(1)
 
